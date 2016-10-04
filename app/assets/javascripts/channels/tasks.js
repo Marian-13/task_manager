fa78c96d662e54f8269b3060e61a1c785cc05a80
Marian <marian144549@gmail.com>
2016-10-04 18:21:11 +0300

    Fixing last issues

--- app/assets/javascripts/channels/tasks.js
+++ app/assets/javascripts/channels/tasks.js
@@ -2,12 +2,16 @@ function fayeSubscribe(senderChannel) {
   // Global
   url = 'http://' + document.location.host + '/faye';
   client = new Faye.Client(url);
+  console.log("Became server client."); // TODO For testing purpose only
 
   subscription = client.subscribe(senderChannel, function(message) {
+    console.log(message.text);
     if (message.text) {
       addReceivedMessageText(message.text);
     }
   });
+
+  console.log("Subscribed to channel " + senderChannel +"."); // TODO For testing purpose only
 }
 
 function addReceivedMessageText(messageText) {
